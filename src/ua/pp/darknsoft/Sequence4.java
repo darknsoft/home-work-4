d998aeea4d24471bc35415e02da8a3413cd6949a
darknsoft <samsonov.omr@gmail.com>
2017-11-04 23:52:13 +0200

    Sequence 4

---
+++ src/ua/pp/darknsoft/Sequence4.java
@@ -0,0 +1,12 @@
+package ua.pp.darknsoft;
+
+public class Sequence4{
+
+	public static void main(String[] args){
+		
+		
+		for(int i=2,j = 0;j <20;i*=2,j++){
+			System.out.print(i + " ");
+		}
+	}
+}
